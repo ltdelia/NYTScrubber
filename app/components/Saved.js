e3efcade957faaa815c24b8fa2df8b3c0de57ab9
ltdelia <delia.louis@gmail.com>
2016-06-18 09:51:38 -0400

    updated Saved Articles page with a Bootstrap panel

--- app/components/Saved.js
+++ app/components/Saved.js
@@ -3,7 +3,19 @@ var React = require('react');
 var Saved = React.createClass({
 	render: function(){
 		return(
-			<p>Saved</p>
+			<div className="row">
+				<div className="col-lg-12">
+
+					<div className="panel panel-primary">
+						<div className="panel-heading">
+							<h1 className="panel-title">Saved Articles</h1>
+						</div>
+						<div className="panel-body">
+							<p>Saved Articles will go here.</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		)
 	}
 });
